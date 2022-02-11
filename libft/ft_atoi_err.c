3aa340d51e35e3ba8f64907602fa59dc2d0d35cb
Jonathan Raffin <jraffin@student.42.fr>
2022-02-11 22:20:31 +0100

    -bool

--- libft/ft_atoi_err.c
+++ libft/ft_atoi_err.c
@@ -6,15 +6,13 @@
 /*   By: jraffin <jraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 11:54:56 by jraffin           #+#    #+#             */
-/*   Updated: 2021/06/25 02:36:16 by jraffin          ###   ########.fr       */
+/*   Updated: 2022/02/11 22:20:10 by jraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdbool.h>
-
 int	ft_atoi_err(const char *str, int *nbr)
 {
-	bool			neg;
+	int				neg;
 	unsigned int	abs;
 
 	if (!*str)
